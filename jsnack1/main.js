5cb0f387213ca60031e5b7ee3dd0ead3f414b2e1
manuelgaleone <galeonemanuel14@gmail.com>
2022-10-12 12:53:31 +0200

    First snack commit

---
+++ jsnack1/main.js
@@ -0,0 +1,14 @@
+//Chiedo due numeri all'Utente
+let userNumber = prompt("Dammi un Numero:");
+let secondUserNumber = prompt("Dammi un altro Numero:");
+
+//Calcolo quale sia il più grande tra i due
+if (userNumber > secondUserNumber) {
+
+    alert("Il numero Maggiore è: " + userNumber);
+
+} else {
+    
+    alert("Il numero Maggiore è: " + secondUserNumber);
+
+};
